935d21c333729a9a631ea42f02260f73aef34d5e
Zachary Juang <zachary822@me.com>
2022-06-07 21:41:18 -0400

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="openapi_to_pydantic",
+    version="0.0.1",
+    author="Zachary Juang",
+    author_email="zacharyjuang@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "pydantic",
+    ],
+)
